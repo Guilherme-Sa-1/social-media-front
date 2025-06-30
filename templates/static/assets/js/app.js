8c285da5cbb0327928658727afbd15424d0ae059
Guilherme Sa <guilhermecgsa7@gmail.com>
2025-06-30 11:50:42 -0300

    Atualizando codigos da estrutura

---
+++ templates/static/assets/js/app.js
@@ -0,0 +1,8 @@
+import {Links} from "./links.js"
+import {Router} from "./router.js"
+
+
+(async ()=>{
+    const root = document.querySelector('#root')
+    await Router(root,window.location.pathname)
+})
